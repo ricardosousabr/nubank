5e273dbfabe0a5069e08491fc7f6467b7b0f4a95
Ricardo <ricardo.br.pi@gmail.com>
2020-12-23 10:49:07 -0300

    refactor(pix) Pix code refactoring

--- src/components/Pix/index.js
+++ src/components/Pix/index.js
@@ -1,5 +1,6 @@
 import './styles.scss';
 import PixImage from '../../img/pix.png';
+import Link from '../Link';
 
 function Pix() {
   return (
@@ -8,13 +9,13 @@ function Pix() {
         <div className="container">
           <div className="row">
             <div className="col-12">
-              <p className="pix__text">Pix: um novo jeito de fazer transferências e pagamentos</p>
-              <a href="#">Saiba como funciona <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z"/><path d="M16.172 11l-5.364-5.364 1.414-1.414L20 12l-7.778 7.778-1.414-1.414L16.172 13H4v-2z"/></svg></a>
+              <p className="pix__title">Pix: um novo jeito de fazer transferências e pagamentos</p>
+              <Link/>
               <div className="text-center">
                 <img className="pix__img" src={PixImage} alt="pix"/>
               </div>
               <div className="pix__account">
-                <h2>Peça sua conta e cartão de crédito do Nubank</h2>
+                <h3 className="pix__text">Peça sua conta e cartão de crédito do Nubank</h3>
                 <input className="pix__input" placeholder="Digite seu CPF" type="text"/>
                 <button className="pix__button"><strong>Continuar</strong><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="none" viewBox="0 0 21 20" class="SVGWrapper-c62y5b-0 ilbcfK" color="currentColor" role="img"><title>Arrow Right</title><g><path d="M18.5 10H2.5M18.5 10L12 16.5M18.5 10L12 3.5" stroke="currentColor" stroke-width="2" stroke-linecap="square" stroke-linejoin="round"></path></g></svg></button>
               </div>
