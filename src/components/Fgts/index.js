2cde76050a45b8d379902d913b887514f58057b8
Ricardo <ricardo.br.pi@gmail.com>
2020-12-23 19:38:26 -0300

    feature(fgts) A new resource to receive your account assistance

---
+++ src/components/Fgts/index.js
@@ -0,0 +1,23 @@
+import './styles.scss'
+import FgtsImage from '../../img/fgts.jpg'
+import Link from '../Link'
+
+function Fgts () {
+  return (
+    <>
+      <section className="fgts">
+        <div className="container">
+          <div className="row">
+            <div className="col-12">
+              <h2 className="fgts__text">Traga seu FGTS e Auxílio Emergencial para o Nubank</h2>
+              <Link />
+              <img className="fgts__img" src={FgtsImage} alt=""/>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default Fgts;
--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Header from './components/Header';
 import Pix from './components/Pix'
+import Fgts from './components/Fgts'
 
 ReactDOM.render(
   <React.StrictMode>
     <Header />
     <Pix />
+    <Fgts />
   </React.StrictMode>,
   document.getElementById('root')
 );
