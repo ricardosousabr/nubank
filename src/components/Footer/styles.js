79f1cdf5c32daec0a091ffb165093543171825db
Ricardo <ricardo.br.pi@gmail.com>
2021-01-09 00:44:57 -0300

    refactor (footer): refactor the footer

--- src/components/Footer/styles.js
+++ src/components/Footer/styles.js
@@ -28,3 +28,31 @@ export const Link = styled.a`
     color: ${colors.white};
   }
 `;
+
+export const LinkSvg = styled.a`
+  margin-left: 40px;
+  margin-bottom: 60px;
+`;
+
+export const DivBorder = styled.div`
+  padding-top: 70px;
+  padding-bottom: 70px;
+  border-top: 1px solid ${colors.white};
+  border-bottom: 1px solid ${colors.white};
+`;
+
+export const SvgApple = styled.svg`
+  margin-bottom: 20px;
+`;
+
+export const SvgPlay = styled.svg`
+  margin-bottom: 90px;
+`;
+
+export const SvgFooter = styled.svg`
+  margin-bottom: 40px;
+`;
+
+export const TextFooter = styled.p`
+  margin-bottom: 40px;
+`;
